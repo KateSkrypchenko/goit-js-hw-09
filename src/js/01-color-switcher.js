9c300e7b0d6a11540d0af89275688979f1996341
user <katalonij440@gmail.com>
2022-11-16 18:25:45 +0200

    add change color body

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+const TIME_CHANGE = 1000;
+let timerId = null;
+
+refs.buttonStart.addEventListener('click', onStartChangeColor);
+refs.buttonStop.addEventListener('click', onStoptChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onGetColorBody() {
+  const colorBody = getRandomHexColor();
+  refs.body.style.backgroundColor = colorBody;
+}
+
+function onStartChangeColor() {
+  refs.buttonStart.disabled = true;
+  timerId = setInterval(onGetColorBody, TIME_CHANGE);
+}
+
+function onStoptChangeColor() {
+  refs.buttonStart.disabled = false;
+  clearInterval(timerId);
+}
